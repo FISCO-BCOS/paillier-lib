d73455225178c3c06e3400991723456558667def
shareong <740310627@qq.com>
2023-11-16 14:30:02 +0800

    remove hex encoding for cipher

--- paillierCpp/callpaillier.h
+++ paillierCpp/callpaillier.h
@@ -1,5 +1,5 @@
 /** @file callpaillier.h
- *  @author shawnhe 
+ *  @author shawnhe
  *  @date 20190816
  */
 
@@ -7,28 +7,28 @@
 #include <exception>
 #include <string>
 
-struct CallException : public std::exception
-{
+struct CallException : public std::exception {
 
 public:
-    CallException(char *e)
-    {
-        myException = e;
-    }
-    virtual char *what()
-    {
-        return myException;
-    }
+  CallException(char *e) { myException = e; }
+  virtual char *what() { return myException; }
 
 private:
-    char *myException;
+  char *myException;
 };
 
-class CallPaillier
-{
-    // Encapsulate call protocol
+class CallPaillier {
+  // Encapsulate call protocol
 public:
-    CallPaillier(){};
-    ~CallPaillier(){};
-    std::string paillierAdd(std::string &cipher1, std::string &cipher2);
+  CallPaillier(){};
+  ~CallPaillier(){};
+
+  // cipher is hex structured
+  std::string paillierAdd(const std::string &cipher1,
+                          const std::string &cipher2);
+
+  // cipher is non-hex structured
+  std::vector<unsigned char>
+  paillierAdd(const std::vector<unsigned char> &cipher1,
+              const std::vector<unsigned char> &cipher2);
 };
